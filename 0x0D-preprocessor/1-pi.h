5cca3ca72180ac14720fdb6f4678de69f510194b
LizzMash <elizabethmashilo@gmail.com>
2022-03-07 14:31:40 +0200

    commit this

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+* File: 0-object_like_macro named
+* Desc: Header file that defines a macro named
+* PI as an abbreviation for token 3.14159265359
+*/
+
+#define PI 3.14159265359
+
+#endif
+
