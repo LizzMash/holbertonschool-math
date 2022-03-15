78d87bc648a15bf237f1729fe342a3e75f0408be
LizzMash <elizabethmashilo@gmail.com>
2022-03-15 11:43:47 +0200

    complex math.

---
+++ holbertonschool-math/0x00-math_complex/2-modulus.c
@@ -0,0 +1,22 @@
+/*
+* 2-modulus.c - Modulus of complex
+* Author: Elizabeth Mashilo
+* Date: March 15, 2022
+*/
+
+#include "holberton.h"
+#include <math.h>
+
+/**
+* modulus - return the modulus
+* @c: structure complex
+* Return: c
+*/
+
+double modulus(complex c)
+{
+	double mod;
+
+	mod = sqrt(pow(c.re, 2) + pow(c.im, 2));
+	return (mod);
+}
